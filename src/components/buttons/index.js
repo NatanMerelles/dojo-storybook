7221e36dfea53e81f1e313ed8d2317ff3a5e959f
Natan Merelles <natanmerelles@gmail.com>
2021-06-07 17:37:25 -0300

    estrutura inicial do botão

---
+++ src/components/buttons/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ButtonBase } from '@material-ui/core';
+
+export const Button = styled(ButtonBase)`
+  padding: 6px 16px;
+`;
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Button } from './Button';
+import { Button } from '../components/buttons';
 
 export default {
   title: 'Example/Button',
@@ -12,25 +12,7 @@ export default {
 
 const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+export const Base = Template.bind({});
+Base.args = {
+  children: 'Botão'
 };
